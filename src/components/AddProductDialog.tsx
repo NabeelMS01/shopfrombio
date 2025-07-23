4bf5782238f7a6040598f52eee64f277e820708e
Nabeel M.S <nabeel.ms78@gmail.com>
2025-07-23 08:52:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/AddProductDialog.tsx
+++ src/components/AddProductDialog.tsx
@@ -17,7 +17,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { addProduct } from '@/app/actions/product';
-import { useToast } from './hooks/use-toast';
+import { useToast } from '@/hooks/use-toast';
 import { cn } from '@/lib/utils';
 
 const initialState = {
